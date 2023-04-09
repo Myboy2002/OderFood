d87be52bcd80834d668b0abf3927ad451a5ffc27
Myboy2002 <tienca0610@gmail.com>
2023-04-10 03:06:54 +0700

    login

--- app/src/main/java/com/example/sairamkrishna/oderfood/Database/CreateDatabase.java
+++ app/src/main/java/com/example/sairamkrishna/oderfood/Database/CreateDatabase.java
@@ -11,6 +11,28 @@ public class CreateDatabase extends SQLiteOpenHelper {
     public static String tb_KhachHang = "KHACHHANG";
     public static String tb_LoaiMon = "LOAIMON";
     public static String tb_MonAn = "MONAN";
+    public static String tb_ChiTietDatMon = "CHITIETDATMON" ;
+
+    public static String tb_KhachHang_MaKH = "MAKH";
+    public static String tb_KhachHang_TenKH = "TENKH";
+    public static String tb_KhachHang_MkKH = "MKKH";
+    public static String tb_KhachHang_GioiTinh = "GIOITINH" ;
+    public static String tb_KhachHang_NgaySinh = "NGAYSINH" ;
+    public static String tb_KhachHang_CMND = "CMND" ;
+
+    public static String tb_LoaiMon_MaLoai = "MALOAI";
+    public static String tb_LoaiMon_TenLoai = "TENLOAI";
+
+    public static String tb_MonAn_MaMonAn = "MAMONAN";
+    public static String tb_MonAn_TenMonAn = "TENMONAN";
+    public static String tb_MonAn_Gia = "GIA";
+    public static String tb_MonAn_MaLoai = "MALOAI";
+
+    public static String tb_ChiTietDatMon_MaDatMon = "MADATMON";
+    public static String tb_ChiTietDatMon_MaMonAn = "MAMONAN";
+    public static String tb_ChiTietDatMon_MaKH = "MAKH";
+    public static String tb_ChiTietDatMon_SoLuong = "SOLUONG";
+    public static String tb_ChiTietDatMon_TongTien = "TONGTIEN";
 
     public CreateDatabase( Context context) {
         /*context, name, factory,version*/
@@ -19,7 +41,15 @@ public class CreateDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String tbKhachhang = ""
+        String tbKhachHang = "CREATE TABLE" + tb_KhachHang + " ( " + tb_KhachHang_MaKH + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + tb_KhachHang_TenKH + " TEXT, " + tb_KhachHang_MkKH + " TEXT, " + tb_KhachHang_GioiTinh + " TEXT, "
+                + tb_KhachHang_NgaySinh + " TEXT, " + tb_KhachHang_CMND + " INTEGER )";
+        String tbLoaiMon = "CREATE TABLE" + tb_LoaiMon + " ( " + tb_LoaiMon_MaLoai + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + tb_LoaiMon_TenLoai + " TEXT )";
+        String tbMonAn = "CREATE TABLE" + tb_MonAn + " ( " + tb_MonAn_MaMonAn + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + tb_MonAn_TenMonAn + " TEXT, " + tb_MonAn_MaLoai + " INTEGER " + tb_MonAn_Gia + " TEXT )";
+        String tbChiTietDatMon = "CREATE TABLE" + tb_ChiTietDatMon + " ( " + tb_ChiTietDatMon_MaDatMon + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + tb_ChiTietDatMon_MaKH + " INTEGER, " + tb_ChiTietDatMon_MaMonAn + " INTEGER, " + tb_ChiTietDatMon_SoLuong  ;
     }
 
     @Override
